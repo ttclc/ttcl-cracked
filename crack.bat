79a8445e6700e3c5d32656d3d5a8e9da718788b6
ttclc <130464043+ttclc@users.noreply.github.com>
2023-04-14 21:45:36 +0300

    Update crack.bat

--- crack.bat
+++ crack.bat
@@ -1,7 +1,7 @@
 @echo off
-%python311-32% pyinsxtractor.py %1
+python pyinsxtractor.py %1
 echo.
-%python311-32% patcher.py %1_extracted\ttClient1.4.pyc cracked_ttcl.pyc
+python patcher.py %1_extracted\ttClient1.4.pyc cracked_ttcl.pyc
 echo.
 echo.
 if %errorlevel% == 0 (
